7df0b3aa872c8e1f7ea3ece38b7224a2797a6e3a
sofian hadiwijaya <me.sofian.hadiwijaya@gmail.com>
2023-02-15 00:59:35 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/sofianhw/go-jetstream-order
+
+go 1.19
+
+require github.com/nats-io/nats.go v1.23.0
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/nats-io/nats-server/v2 v2.9.14 // indirect
+	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ monitor/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"runtime"
+
+	"github.com/nats-io/nats.go"
+
+	"github.com/sofianhw/go-jetstream-order/model"
+)
+
+func main() {
+	// Connect to NATS
+	nc, _ := nats.Connect(nats.DefaultURL)
+	js, err := nc.JetStream()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Create durable consumer monitor
+	js.Subscribe("ORDERS.*", func(msg *nats.Msg) {
+		msg.Ack()
+		var order model.Order
+		err := json.Unmarshal(msg.Data, &order)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("\nmonitor service subscribes from subject:%s\n", msg.Subject)
+		log.Printf("OrderID:%d, CustomerID: %s, Status:%s\n\n", order.OrderID, order.CustomerID, order.Status)
+	}, nats.Durable("monitor"), nats.ManualAck())
+
+	runtime.Goexit()
+
+}
